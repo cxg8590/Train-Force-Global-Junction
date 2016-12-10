32644e128f730bc47f7e778cc88e2e33fb87d9d4
Christian Granville (RIT Student) <cxg8590@ad.rit.edu>
2016-12-09 21:37:40 -0500

    Did some server stuff

--- TrainForce/js/loader.js
+++ TrainForce/js/loader.js
@@ -18,38 +18,3 @@ function loadImagesWithCallback(sources, callback) {
 	  imageObjects[i].src = sources[i];
 	}
  }
-  
- /* window.onload = function(){
-	console.log("window.onload called");
-	
-	// Preload Images and Sound
-	app.queue = new createjs.LoadQueue(false);
-	app.queue.installPlugin(createjs.Sound);
-	app.queue.on("complete", function(e){
-		console.log("images loaded called");
-		app.blastem.init(app.ship);
-	});
-
-	app.queue.loadManifest([
-     {id: "shipImage", src:"images/Hunter1.png"},
-     {id: "enemyImage", src:"images/Drone1.png"},
-     {id: "explosionImage", src:"images/explosion.png"},
-     {id: "explosionImage2", src:"images/explosion2.png"},
-     {id: "explosionImage3", src:"images/explosion3.png"},
-     {id:"bullet", src:"sounds/laser4.mp3"},
-	 {id:"explosion", src:"sounds/fireball4.mp3"},
-	 {id:"soundtrack", src:"sounds/soundtrack.mp3"}
-	]);
-
-	// event listeners
-	window.addEventListener("keydown",function(e){
-		//console.log("keydown=" + e.keyCode);
-		app.keydown[e.keyCode] = true;		
-	});
-		
-	window.addEventListener("keyup",function(e){
-		//console.log("keyup=" + e.keyCode);
-		app.keydown[e.keyCode] = false;
-		if(e.keyCode == 83) app.blastem.toggleSoundtrack(); // s
-	});
-}*/
--- TrainForce/src/server.js
+++ TrainForce/src/server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 const socketio = require('socket.io');
+const express = require('express');
+const path = require('path');
+//const routes = require('routes');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const index = fs.readFileSync('TrainForce.html');
+const index = fs.readFileSync('client/index.html');
 
 const onRequest = (request, response) => {
 	response.writeHead(200, {'Content-Type': 'text/html'});
@@ -12,15 +15,55 @@ const onRequest = (request, response) => {
 	response.end();
 };
 
+
 var id = 0;
 var room = "room1";
 
-const app = http.createServer(onRequest).listen(port);
+const app = express();
+const server = app.listen(port);
+const io = require('socket.io').listen(server);
 
+app.configure(
+  /*app.set('views', __dirname + '/views'),
+  app.set('view engine', 'jade'),
+  app.use(express.favicon()),
+  app.use(express.logger('dev')),
+  app.use(express.static(__dirname + '/public')),
+  app.use(express.bodyParser()),
+  app.use(express.methodOverride()),
+  app.use(app.router)*/
+);
+
+
+//app.get('/', routes.index);
 console.log(`Listening on 127.0.0.1: ${port}`);
 
-const io = socketio(app);
+/*const app = http.createServer(onRequest).listen(port);
+const exp = express();
+exp.use('/TF', express.static(path.resolve(`${__dirname}`)));*/
 
+//const app = http.createServer(onRequest).listen(port);
+/*const app = require('express')();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+
+//console.log("index: " + index);
+
+app.listen(port);
+
+app.get('/', function(req, res) {
+   res.sendfile(index); 
+    res.send();
+});
+//app.use('/', express.static(path.resolve(index)));
+
+console.log(`Listening on 127.0.0.1: ${port}`);
+//console.log(`Listening on 127.0.0.1: ${port}`);
+
+//const io = socketio(app);
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const onJoined = (sock) => {
 	const socket = sock;
 	
